761cc1d365a323041612a58f40e1e9e7467b43b6
julianamschwand <j.amschwand@hotmail.com>
2025-07-15 23:36:13 +0200

    Implemented the song download function

--- src/views/DownloadView.vue
+++ src/views/DownloadView.vue
@@ -1,9 +1,37 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { loginState } from '@/api/routes/user-routes.js'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import { downloadSong } from '@/api/routes/song-routes'
 
 const router = useRouter()
+const url = ref("")
+const downloading = ref(false)
+const errorMessage = ref("")
+const successMessage = ref("")
+
+const handleDownload = async () => {
+  if (!url.value.trim()) {
+    errorMessage.value += "Enter a URL"
+    return
+  }
+
+  const tempUrl = url.value
+  url.value = ""
+
+  downloading.value = true
+  const response = await downloadSong(tempUrl)
+
+  if (response.success) {
+    errorMessage.value = ""
+    successMessage.value = response.message
+  } else {
+    successMessage.value = ""
+    errorMessage.value = response.message
+  }
+
+  downloading.value = false
+}
 
 onMounted(async () => {
   const response = await loginState()
@@ -12,7 +40,85 @@ onMounted(async () => {
 </script>
 <template>
   <header>Download</header>
+  <div class="main-container">
+    <div id="center-container">
+      <h1>Enter Song- or Playlist-URL</h1>
+      <div id="downloadbar-container">
+        <div><input type="text" placeholder="URL" v-model="url" @keydown.enter="handleDownload"></div>
+        <div v-if="downloading">
+          <div class="loader-download"></div>
+        </div>
+        <div id="download-button" class="button button-dark-hover" @click="handleDownload" v-if="!downloading">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/>
+          </svg>
+        </div>
+      </div>
+      <div class="error-message" v-if="errorMessage">{{ errorMessage }}</div>
+      <div class="success-message" v-if="successMessage">{{ successMessage }}</div>
+    </div>
+  </div>
 </template>
 <style scoped>
+.main-container {
+  height: calc(100% - calc(var(--ui-scale)) * 0.8 * 2)
+}
 
+#center-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: calc(var(--ui-scale) * 0.1);
+}
+
+#downloadbar-container {
+  width: calc(var(--ui-scale) * 3.5);
+  display: flex;
+  align-items: center;
+  gap: calc(var(--ui-scale) * 0.05);
+}
+
+#downloadbar-container > div{
+  display: flex;
+  align-items: center;
+  padding: 0px;
+  background-color: var(--objects);
+  justify-content: center;
+  width: calc(var(--ui-scale) * 0.4);
+  height: calc(var(--ui-scale) * 0.4);
+  box-sizing: border-box;
+  border-radius: calc(var(--ui-scale) * 0.05);
+}
+
+#downloadbar-container > div:first-child {
+  width: calc(100% - calc(var(--ui-scale) * 0.45));
+  padding-left:calc(var(--ui-scale) * 0.1);
+  padding-right: calc(var(--ui-scale) * 0.1);
+}
+
+#downloadbar-container input {
+  background-color: var(--objects);
+  font-size: calc(var(--ui-scale) * 0.15);
+  border: none;
+  outline: none;
+  width: 100%;
+}
+
+svg {
+  height: calc(var(--ui-scale) * 0.24);
+  width: calc(var(--ui-scale) * 0.24);
+  fill: var(--background);
+}
+
+h1 {
+  font-size: calc(var(--ui-scale) * 0.27);
+}
+
+#download-button {
+  background-color: var(--accent) !important;
+}
+
+.loader-download {
+  background-color: var(--accent) !important;
+}
 </style>
